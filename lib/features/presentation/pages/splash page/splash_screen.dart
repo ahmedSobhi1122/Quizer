9406d7dc5479e298f3f70c71bfe3403da248a553
ahmedSobhi1122 <99087755+ahmedSobhi1122@users.noreply.github.com>
2024-09-23 18:08:43 +0300

    some edit

--- lib/features/presentation/pages/splash page/splash_screen.dart
+++ lib/features/presentation/pages/splash page/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:quizer/core/helper/extensions.dart';
 import 'package:quizer/core/resources/app_colors.dart';
 import 'package:quizer/core/resources/app_fonts.dart';
 import 'package:quizer/core/resources/assets_manager.dart';
@@ -11,9 +12,10 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Future.delayed(const Duration(seconds: 3), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const SelectionScreen()),
-      );
+      // Navigator.of(context).pushReplacement(
+      //   MaterialPageRoute(builder: (context) => const SelectionScreen()),
+      // );
+      context.pushReplacementNamed(Routes.selectionScreenRoute);
     });
 
     return Scaffold(
@@ -21,7 +23,11 @@ class SplashScreen extends StatelessWidget {
         width: double.infinity,
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors: [AppColors.purpleColor, Color(0xffCDC4FB), AppColors.whiteColor],
+            colors: [
+              AppColors.purpleColor50,
+              AppColors.purpleColor30,
+              AppColors.purpleColor10,
+            ],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           ),
@@ -40,19 +46,17 @@ class SplashScreen extends StatelessWidget {
                   children: [
                     Image.asset(
                       ImageAssets.logo,
-                      width: 140,
-                      height: 140,
+                      width: context.width * 0.326,
+                      height: context.width * 0.326,
                     ),
                     const SizedBox(height: 20),
                     const Text(
                       'Quizzer',
                       style: TextStyle(
-
                           color: Colors.black,
                           fontSize: FontSize.f22,
                           fontFamily: FontConstants.primaryEnglishFont,
-                          fontWeight: FontWeight.bold
-                      ),
+                          fontWeight: FontWeight.bold),
                     ),
                   ],
                 ),
@@ -63,7 +67,8 @@ class SplashScreen extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation<Color>(AppColors.purpleColor),
+                      valueColor:
+                          AlwaysStoppedAnimation<Color>(AppColors.purpleColor),
                     ),
                     SizedBox(height: 20),
                   ],

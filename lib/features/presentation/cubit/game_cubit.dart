2c09bc4d6956e4949a2d0d350cefa71f3444aba6
ahmedSobhi1122 <99087755+ahmedSobhi1122@users.noreply.github.com>
2024-10-20 02:35:50 +0300

    solve bug quiz

--- lib/features/presentation/cubit/game_cubit.dart
+++ lib/features/presentation/cubit/game_cubit.dart
@@ -4,33 +4,34 @@ import '../../data_sources/models/question_model.dart';
 import '../state/game_state.dart';
 
 class GameCubit extends Cubit<GameState> {
-
-  GameCubit(List<QuestionModel?> questions) : super(GameState(
-    questions: questions,
-    currentQuestionIndex: 0,
-    selectedAnswers: List<String?>.filled(questions.length, null),
-  ));
+  GameCubit(List<QuestionModel?> questions)
+      : super(GameState(
+          questions: questions,
+          currentQuestionIndex: 0,
+          selectedAnswers: List<String?>.filled(questions.length, null),
+        ));
 
   // Function to navigate to the next question
   void goToNextQuestion() {
     if (state.currentQuestionIndex < state.questions.length - 1) {
-      emit(state.copyWith(currentQuestionIndex: state.currentQuestionIndex + 1));
+      emit(
+          state.copyWith(currentQuestionIndex: state.currentQuestionIndex + 1));
     }
   }
 
   // Function to navigate to the previous question
   void goToPreviousQuestion() {
     if (state.currentQuestionIndex > 0) {
-      emit(state.copyWith(currentQuestionIndex: state.currentQuestionIndex - 1));
+      emit(
+          state.copyWith(currentQuestionIndex: state.currentQuestionIndex - 1));
     }
   }
 
   // Function to select an answer for a question
-  void selectAnswer(int questionIndex, String selectedAnswer) {
+  void selectAnswer(int questionIndex, String? selectedAnswer) {
     final updatedAnswers = List<String?>.from(state.selectedAnswers);
-    updatedAnswers[questionIndex] = selectedAnswer;
+    updatedAnswers[questionIndex] = selectedAnswer ?? "";
 
     emit(state.copyWith(selectedAnswers: updatedAnswers));
   }
 }
-
